6495d958de1401e2a66282407f9a13e47eebbb3e
mirmunna <124150221+mirmunna@users.noreply.github.com>
2025-06-15 08:32:54 +0530

    Added sw.js
    Sw.js

---
+++ sw.js
@@ -0,0 +1,20 @@
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open('kiryana-store').then(cache => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/style.css',
+        '/app.js',
+        '/manifest.json'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => response || fetch(e.request))
+  );
+});
